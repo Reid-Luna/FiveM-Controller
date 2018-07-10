9cb917b2d7687d10fa07031b108be3749eaf6026
= <reidluna@icloud.com>
2018-07-10 16:25:36 -0600

    fixing

--- frontend/src/reducers/statusReducer.js
+++ frontend/src/reducers/statusReducer.js
@@ -1,11 +1,12 @@
 import { SET_STATUS } from "../actions/types";
 
-const initialState = null;
+const initialState = {};
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case SET_STATUS:
       return {
+        ...state,
         status: action.payload.status
       };
     default:
